292e637e5076c46b8bec35bd060f5c5168902fbd
Tracey Bullington <traceylbullington@gmail.com>
2023-06-09 10:55:34 -0500

    Initial Commit

---
+++ willThisWork/willThisWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  willThisWorkApp.swift
+//  willThisWork
+//
+//  Created by scholar on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct willThisWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
